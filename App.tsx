19f37c23ed71236f7f5b05416efa1735d66670ec
abass-dev <cheikabassben@gmail.com>
2023-03-23 23:26:56 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import { useEffect, useState } from 'react'
+
+export default function App() {
+const [name, setName] = useState()
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+  </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-gbqvcf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-gbqvcf)
